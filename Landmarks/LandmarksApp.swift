f98c1fb7a5d9d3bf7a63f5b4b95d83b2119878d4
David Xiao <dixiao@email.arizona.edu>
2021-10-13 12:08:00 -1000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by David Xiao on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
